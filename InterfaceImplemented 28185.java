2b3b4341ff3f352583a4ab9e25df8a1e67f7e0f9
jjalmeda23 <135943014+jjalmeda23@users.noreply.github.com>
2023-06-08 11:00:10 +0530

    Delete InterfaceImplemented 28185.java

--- InterfaceImplemented 28185.java
+++
@@ -1,8 +0,0 @@
-package pkginterface;
-public class InterfaceImplemented implements MyFirstInterface {
-    
-    @Override
-    public void display(){
-        System.out.println(x); //Error : cannot assign a valu to final variable "x"
-}
-}
